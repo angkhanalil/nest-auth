de93d6e71692db4a86862b6cd68870285dd70160
Angkhana <34016307+angkhanalil@users.noreply.github.com>
2024-07-10 15:53:17 +0700

    test feature commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +8,7 @@ export class AuthService {
     private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
+
   async signIn(
     username: string,
     pass: string,
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -15,4 +15,32 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findOne', () => {
+    it('should return a user if found', async () => {
+      const username = 'john';
+      const user = await service.findOne(username);
+      expect(user).toEqual({
+        userId: 1,
+        username: 'john',
+        password: 'changeme', //expect.any(String), // The password is hashed, so we just expect a string
+      });
+    });
+
+    it('should return undefined if user is not found', async () => {
+      const username = 'nonexistent';
+      const user = await service.findOne(username);
+      expect(user).toBeUndefined();
+    });
+  });
+
+  it('should return a user if found', async () => {
+    const username = 'john';
+    const user = await service.findOne(username);
+    expect(user).toEqual({
+      userId: 1,
+      username: 'john',
+      password: 'changeme', //expect.any(String), // The password is hashed, so we just expect a string
+    });
+  });
 });
